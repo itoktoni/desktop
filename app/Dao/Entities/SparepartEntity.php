c4d1a424e8cb5065e9af5fc9cd8b35ee25445587
azzm17_ <azzama151217@gmail.com>
2022-07-09 20:54:12 +0700

    create master sparepart

---
+++ app/Dao/Entities/SparepartEntity.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Dao\Entities;
+
+trait SparepartEntity
+{
+    public static function field_code()
+    {
+        return 'sparepart_id';
+    }
+
+    public function getFieldCodeAttribute()
+    {
+        return $this->{$this->field_code()};
+    }
+
+    public static function field_name()
+    {
+        return 'sparepart_name';
+    }
+
+    public function getFieldNameAttribute()
+    {
+        return $this->{$this->field_name()};
+    }
+
+    public static function field_location_id()
+    {
+        return 'sparepart_location_id';
+    }
+
+    public function getFieldLocationIdAttribute()
+    {
+        return $this->{$this->field_location_id()};
+    }
+
+    public static function field_description()
+    {
+        return 'sparepart_description';
+    }
+
+    public function getFieldDescriptionAttribute()
+    {
+        return $this->{$this->field_description()};
+    }
+
+    public static function field_stock()
+    {
+        return 'sparepart_stock';
+    }
+
+    public function getFieldStockAttribute()
+    {
+        return $this->{$this->field_stock()};
+    }
+}
