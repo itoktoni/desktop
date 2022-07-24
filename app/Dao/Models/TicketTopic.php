37dd1680aa3a8b837a31801ec5337917356fa1d9
azzm17_ <azzama151217@gmail.com>
2022-07-24 15:56:28 +0700

    update transaksi ticket
    update entity code to primary

--- app/Dao/Models/TicketTopic.php
+++ app/Dao/Models/TicketTopic.php
@@ -43,7 +43,7 @@ class TicketTopic extends Model
     public function fieldDatatable(): array
     {
         return [
-            DataBuilder::build($this->field_code())->name('ID'),
+            DataBuilder::build($this->field_primary())->name('ID'),
             DataBuilder::build($this->field_name())->name('Name')->sort(),
             DataBuilder::build($this->field_active())->name('Active')->show(false),
         ];
