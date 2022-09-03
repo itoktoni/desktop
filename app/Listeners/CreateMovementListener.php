5e1d056ec04c6837c121b4778ce18e2c94e35c02
Ryas-Yusenda <44300598+Ryas-Yusenda@users.noreply.github.com>
2022-09-03 13:40:31 +0700

    update whatsup notification

--- app/Listeners/CreateMovementListener.php
+++ app/Listeners/CreateMovementListener.php
@@ -2,12 +2,13 @@
 
 namespace App\Listeners;
 
+use App\Dao\Enums\MovementStatus;
 use App\Dao\Models\Location;
 use App\Dao\Models\Product;
-use App\Dao\Models\User;
 use App\Events\CreateMovementEvent;
 use App\Mail\CreateMovementEmail;
 use Illuminate\Support\Facades\Mail;
+use Plugins\WhatsApp;
 
 class CreateMovementListener
 {
@@ -29,14 +30,27 @@ class CreateMovementListener
      */
     public function handle(CreateMovementEvent $event)
     {
-        $report_from = auth()->user()->{User::field_email()} ?? false;
-        $report_to = $event->data->has_user->{User::field_email()} ?? false;
-        $product_name = $event->data->has_product->{Product::field_name()} ?? '-';
-        $location_old = $event->data->has_location_old->{Location::field_name()} ?? '-';
-        $location_new = $event->data->has_location->{Location::field_name()} ?? '-';
+        $report_from = auth()->user() ?? false;
+        $report_to = $event->data->has_user ?? false;
 
-        if ($report_to) {
-            Mail::to([$report_from, $report_to])->send(new CreateMovementEmail($event->data, $product_name, $location_old, $location_new));
+        $email_from = $report_from->field_email ?? false;
+        $email_to = $report_to->field_email ?? false;
+
+        $phone_from = $report_from->field_phone ?? false;
+        $phone_to = $report_to->field_phone ?? false;
+
+        if ($email_to) {
+            $product_name = $event->data->has_product->{Product::field_name()} ?? '-';
+            $location_old = $event->data->has_location_old->{Location::field_name()} ?? '-';
+            $location_new = $event->data->has_location->{Location::field_name()} ?? '-';
+
+            Mail::to([$email_from, $email_to])->send(new CreateMovementEmail($event->data, $product_name, $location_old, $location_new));
         }
+
+        if ($phone_from) {
+            $message = MovementStatus::getDescription((int) $event->data->field_status) . '  -  ' . $event->data->field_description;
+            WhatsApp::send($phone_from, $message);
+        }
+
     }
 }
