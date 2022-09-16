703cc1a298f48fc87d57dda8df0c19ba129981d5
Itoktoni <itok@kidsoft.com.au>
2022-09-16 19:53:25 +0700

    update role

--- app/Http/Requests/RolesRequest.php
+++ app/Http/Requests/RolesRequest.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Requests;
 
+use App\Dao\Models\Roles;
 use App\Dao\Traits\ValidationTrait;
 use Illuminate\Foundation\Http\FormRequest;
 
 class RolesRequest extends FormRequest
 {
     use ValidationTrait;
+
+    public function prepareForValidation()
+    {
+        $this->merge([
+            Roles::field_primary() => strtolower($this->role_name)
+        ]);
+    }
 
     public function validation() : array
     {
